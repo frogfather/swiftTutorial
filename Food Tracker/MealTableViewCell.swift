0eda60b0e105cb30eacd8d54970f13ab1caa1fd0
John Alexander <johnPersonal@unknown.home>
2018-12-13 19:25:46 +0000

    controller connected to class

---
+++ Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  Food Tracker
+//
+//  Created by John Alexander on 12/12/2018.
+//  Copyright © 2018 John Campbell. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
